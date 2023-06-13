388bf7d8bb26e6046e2bbd9c2c8d1558e755a943
T1 <126980610+tadash10@users.noreply.github.com>
2023-06-12 23:50:51 -0300

    Update v2.py

--- v2.py
+++ v2.py
@@ -3,6 +3,8 @@ import sys
 import socket
 import logging
 import subprocess
+import smtplib
+from email.mime.text import MIMEText
 
 def set_iptables_rule():
     # Set iptables rule to block Nmap scans
@@ -32,6 +34,7 @@ def listen_for_nmap_scans(port):
         try:
             client_socket, client_address = listener.accept()
             logging.warning("Nmap scan detected from: %s", client_address[0])
+            send_email_notification(client_address[0])
             client_socket.close()
         except socket.error as e:
             sys.exit("Error accepting incoming connection: " + str(e))
@@ -40,6 +43,62 @@ def setup_logging():
     # Configure logging parameters
     log_format = "%(asctime)s - %(levelname)s - %(message)s"
     logging.basicConfig(filename='nmap_defender.log', level=logging.WARNING, format=log_format)
+
+def send_email_notification(ip_address):
+    # Send email notification to the administrator
+    from_address = 'sender@example.com'
+    to_address = 'admin@example.com'
+    subject = 'Nmap Scan Detected'
+    body = f'Nmap scan detected from IP address: {ip_address}'
+    
+    # Create the email message
+    msg = MIMEText(body)
+    msg['From'] = from_address
+    msg['To'] = to_address
+    msg['Subject'] = subject
+    
+    try:
+        with smtplib.SMTP('smtp.example.com', 587) as smtp_server:
+            smtp_server.starttls()
+            smtp_server.login('username', 'password')
+            smtp_server.send_message(msg)
+            logging.info("Email notification sent")
+    except smtplib.SMTPException as e:
+        logging.error("Failed to send email notification: %s", str(e))
+
+def whitelist_ip_address(ip_address):
+    # Add the given IP address to the whitelist
+    subprocess.run(["iptables", "-I", "INPUT", "-s", ip_address, "-j", "ACCEPT"], check=True)
+    logging.info("IP address %s added to the whitelist.", ip_address)
+
+def rate_limit_connections(ip_address, limit):
+    # Set up rate limiting for the given IP address
+    subprocess.run(["iptables", "-I", "INPUT", "-p", "tcp", "-m", "state", "--state", "NEW",
+                    "-m", "recent", "--set", "--name", "ratelimit", "--rsource"], check=True)
+    subprocess.run(["iptables", "-I", "INPUT", "-p", "tcp", "-m", "state", "--state", "NEW",
+                    "-m", "recent", "--update", "--name", "ratelimit", "--rsource", "--seconds", "60",
+                    "--hitcount", str(limit), "-j", "DROP"], check=True)
+    logging.info("Rate limiting set up for IP address: %s", ip_address)
+
+def integrate_with_siem(event):
+    # Send the event to the SIEM system for centralized monitoring
+    siem_endpoint = 'https://siem.example.com/api/events'
+    
+    try:
+        # Send the event data to the SIEM endpoint using appropriate request libraries or frameworks
+        # Replace the following code with the actual implementation
+        response = requests.post(siem_endpoint, json=event)
+        if response.status_code == 200:
+            logging.info("Event sent to SIEM system.")
+        else:
+            logging.error("Failed to send event to SIEM system. Status code: %d", response.status_code)
+    except Exception as e:
+        logging.error("Failed to send event to SIEM system: %s", str(e))
+
+def dynamic_port_selection():
+    # Implement the logic to dynamically select the listening port based on availability or configuration
+    # Replace the following code with the actual implementation
+    return 8080
 
 def run_defender():
     # Set up logging
@@ -51,9 +110,11 @@ def run_defender():
 
     try:
         set_iptables_rule()
-        listen_for_nmap_scans(80)  # Change the port as required
+        port = dynamic_port_selection()
+        listen_for_nmap_scans(port)
     finally:
         clear_iptables_rule()
 
 if __name__ == "__main__":
     run_defender()
+
